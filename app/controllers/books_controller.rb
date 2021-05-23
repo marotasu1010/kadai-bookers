93b5e64ccb085419ac8d4307a7a533a1b61fbddd
marotasu1010 <happyboxfavor@yahoo.co.jp>
2021-05-23 13:28:31 +0000

    done

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    id = params[:id]
+    @book = Book.find(id)
+  end
+
+  def new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:success] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    else
+      flash[:alert] = 'error:' + book.errors.full_messages.to_s
+      redirect_to books_path
+    end
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:success] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    else
+      flash[:alert] = 'error:' + book.errors.full_messages.to_s
+      redirect_to books_path
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
